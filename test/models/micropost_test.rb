f88425120e7a201c72bc472e290fe8968785a8a9
sheitanos <fabien.poher45@gmail.com>
2018-02-19 10:21:09 +0100

    fix

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -3,18 +3,19 @@ require 'test_helper'
 class MicropostTest < ActiveSupport::TestCase
 
   def setup
-    @user = users(:michael)
-    @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
-  end
+     @user = users(:michael)
+     @micropost = @user.microposts.build(content: "Lorem ipsum")
+   end
 
-  test "should be valid" do
-    assert @micropost.valid?
-  end
 
-  test "user id should be present" do
-    @micropost.user_id = nil
-    assert_not @micropost.valid?
-  end
+ test "should be valid" do
+   assert @micropost.valid?
+ end
+
+ test "user id should be present" do
+   @micropost.user_id = nil
+   assert_not @micropost.valid?
+ end
 
   test "content should be present" do
     @micropost.content = "   "
